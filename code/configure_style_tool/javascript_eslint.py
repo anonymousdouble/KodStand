24c62b543e1dc7d9897ce322974c7f32b247fbce
Gantasy <dynastyg@163.com>
2024-04-18 14:52:22 +0800

    save as proj

--- code/configure_style_tool/javascript_eslint.py
+++ code/configure_style_tool/javascript_eslint.py
@@ -1,38 +1,46 @@
 import subprocess
 import os
 import shutil
-from tqdm import tqdm
-from concurrent.futures import ThreadPoolExecutor
-ignore_dirs = ["node_modules","plugins"]
+import shutil
+import time
+from multiprocessing import Pool
+ignore_dirs = ["node_modules", "plugins"]
 config_path = "C:\\Users\\shuli\\workspace\\KodStand\\data\\eslint\\eslint.config.mjs"
-def check_project(proj_root, save_path):
+
+
+def check_project(proj_root, save_root):
     shutil.copyfile(config_path, os.path.join(proj_root, "eslint.config.mjs"))
-    # create thread pool
-    with ThreadPoolExecutor(max_workers=16) as executor:
-        with open(save_path, "w") as of:
-            for root, dirs, files in os.walk(proj_root):
-                flag = False
-                for item in ignore_dirs:
-                    if item in root:
-                        flag = True
-                        break
-                if flag:
-                    continue
-                for file in files:
-                    if file.endswith(".js"):
-                        file_path = os.path.join(root, file)
-                        print(f"checking {file_path}")
-                        cmd = f"cd {root} && eslint {file_path}"
-                        # params = ["cd",
-                        #     root,
-                        #     "&&",
-                        #     "eslint",
-                        #     "--config",
-                        #     "C:\\Users\\shuli\\workspace\\KodStand\\data\\eslint\\eslint.config.mjs",
-                        #     file_path]
-                        # print(params)
-                        subprocess.run(cmd, shell=True, stdout=of, stderr=of)
-                        # subprocess.run(["eslint", "--no-ignore",file_path], shell=True, stdout=of, stderr=of)
+    shutil.rmtree(save_root, ignore_errors=True)
+    os.makedirs(save_root, exist_ok=True)
+    pool = Pool(24)
+    tasks = []
+    t = time.time()
+    for root, dirs, files in os.walk(proj_root):
+        flag = False
+        for item in ignore_dirs:
+            if item in root:
+                flag = True
+                break
+        if flag:
+            continue
+        current_root = root.replace(proj_root, save_root)
+        os.makedirs(current_root, exist_ok=True)
+        for file in files:
+            if file.endswith(".js"):
+                analyze_fpath = os.path.join(current_root, file).replace(".js", ".txt")
+                file_path = os.path.join(root, file)
+                tasks.append(pool.apply_async(analyze, args=(root, analyze_fpath, file_path)))
+    pool.close()
+    pool.join()
+    print(f"finished {proj_root} in {time.time()-t} seconds")
+
+def analyze(root, analyze_fpath, file_path):
+    print(f"checking {file_path}")
+    cmd = f"eslint {file_path}"
+    with open(analyze_fpath, "w") as of:
+        p = subprocess.Popen(cmd, shell=True, stdout=of, stderr=of, cwd=root)
+        p.wait()
+
 
 def rename_remove_space(proj_root):
     for root, dirs, files in os.walk(proj_root):
@@ -50,16 +58,16 @@ def rename_remove_space(proj_root):
                     if os.path.exists(new_file_path):
                         new_file_path = new_file_path.replace(".js", "_1.js")
                     os.rename(file_path, new_file_path)
+
+
 if __name__ == '__main__':
     repository = "C:\\Users\\shuli\\workspace\\codestandard_data\\javaScriptrepos"
     save_root = os.path.join(os.getcwd(), "data", "eslint_airbnb_result")
     if not os.path.exists(save_root):
         os.makedirs(save_root)
-    
-    rename_remove_space(repository)
-
+    # rename_remove_space(repository)
     for proj in os.listdir(repository):
         proj_root = os.path.join(repository, proj)
-        save_path = os.path.join(save_root, f"{proj}.txt")
+        save_proj_root = os.path.join(save_root, proj)
         print(f"start check proj: {proj}")
-        check_project(proj_root, save_path)
+        check_project(proj_root, save_proj_root)
